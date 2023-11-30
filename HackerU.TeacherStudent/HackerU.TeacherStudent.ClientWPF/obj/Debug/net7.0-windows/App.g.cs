858de9bdec03a14c3a0ae6c49a49b04f80998047
Oz Ifrache <oz99live@gmail.com>
2023-11-30 19:26:56 +0200

    oz3

--- HackerU.TeacherStudent/HackerU.TeacherStudent.ClientWPF/obj/Debug/net7.0-windows/App.g.cs
+++ HackerU.TeacherStudent/HackerU.TeacherStudent.ClientWPF/obj/Debug/net7.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace HackerU.TeacherStudent.ClientWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace HackerU.TeacherStudent.ClientWPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public static void Main() {
             HackerU.TeacherStudent.ClientWPF.App app = new HackerU.TeacherStudent.ClientWPF.App();
             app.InitializeComponent();
