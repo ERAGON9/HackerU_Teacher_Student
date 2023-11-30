bac7f142ef67a14bd1b0375e224a7a974fb49b1e
Oz Ifrache <oz99live@gmail.com>
2023-11-30 19:40:49 +0200

    oz1

--- HackerU.TeacherStudent/HackerU.TeacherStudent.ClientWPF/obj/Debug/net7.0-windows/HackerU.TeacherStudent.ClientWPF.AssemblyInfo.cs
+++ HackerU.TeacherStudent/HackerU.TeacherStudent.ClientWPF/obj/Debug/net7.0-windows/HackerU.TeacherStudent.ClientWPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackerU.TeacherStudent.ClientWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aca43b4e03ec0fdcaa316ed90290ce113472209")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackerU.TeacherStudent.ClientWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackerU.TeacherStudent.ClientWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
